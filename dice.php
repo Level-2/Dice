21878494923cdd74b3f3990228cb754559ed334d
Tom Butler <tom@r.je>
2013-03-14 12:49:32 +0000

    Updated several XML tag names to match the DiceRule format

--- dice.php
+++ dice.php
@@ -123,11 +123,13 @@ class DiceXmlLoader {
 		else return (strpos((string) $str, '{') === 0) ? array(new DiceXmlCallback($str), 'create') : (string) $str;
 	}
 
-	public function loadXml(SimpleXmlElement $map, Dice $dic) {
+	public function loadXml($map, Dice $dic) {
+		if (!($map instanceof SimpleXmlElement)) $map = simplexml_load_file($map);
 		$rules = array();
 		foreach ($map as $key => $value) {
 			$rule = clone $dic->getRule((string) $value->name);				
 			$rule->shared = ($value->shared == 'true');
+			$rule->inherit = ($value->inherit == 'true');
 			if ($value->call) {
 				foreach ($value->call as $name => $call) {
 					$callArgs = array();
@@ -137,7 +139,7 @@ class DiceXmlLoader {
 			}
 			if ($value->instanceof) $rule->instanceOf = (string) $value->instanceof;
 			if ($value->newinstances) $rule->newInstances = explode(',', $value->newinstances);
-			if ($value->use) foreach ($value->use as $use) $rule->substitutions[(string) $use->as] = $this->getComponent((string) $use->class, true);
+			if ($value->substitute) foreach ($value->use as $use) $rule->substitutions[(string) $use->as] = $this->getComponent((string) $use->use, true);
 			if ($value->construct) 	foreach ($value->construct->children() as $child) $rule->constructParams[] = $this->getComponent((string) $child);
 			$dic->addRule((string) $value->name, $rule);
 		}
