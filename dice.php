ab94a235f7c95a5f7ba74157f4858e3eacbf7ed7
TomBZombie <tom@r.je>
2014-01-20 15:59:56 +0000

    Change-Id: I04ceb77a86bd7e0bb14294428e1518f82b3866af

--- dice.php
+++ dice.php
@@ -1,4 +1,5 @@
 <?php 
+namespace Dice;
 /* @description 	Dice - A minimal Dependency Injection Container for PHP  
  * @author 			Tom Butler tom@r.je
  * @copyright  		2012-2013 Tom Butler <tom@r.je>
@@ -14,7 +15,7 @@ class Dice {
 		$this->instances[strtolower(get_class($object))] = $object;
 	}
 	
-	public function addRule($name, DiceRule $rule) {
+	public function addRule($name, Rule $rule) {
 		$rule->substitutions = array_change_key_case($rule->substitutions);
 		$this->rules[strtolower($name)] = $rule;
 	}
@@ -24,13 +25,13 @@ class Dice {
 		foreach ($this->rules as $key => $value) {
 			if ($key !== '*' && is_subclass_of($name, $key) && $value->inherit == true) return $value;
 		}
-		return isset($this->rules['*']) ? $this->rules['*'] : new DiceRule;
+		return isset($this->rules['*']) ? $this->rules['*'] : new Rule;
 	}
 	
 	public function create($component, array $args = array(), $callback = null, $forceNewInstance = false) {
-		if ($component instanceof DiceInstance) $component = $component->name;
+		if ($component instanceof Instance) $component = $component->name;
 		
-		if (!isset($this->rules[strtolower($component)]) && !class_exists($component)) throw new Exception('Class does not exist for creation: ' . $component);
+		if (!isset($this->rules[strtolower($component)]) && !class_exists($component)) throw new \Exception('Class does not exist for creation: ' . $component);
 		
 		if (!$forceNewInstance && isset($this->instances[strtolower($component)])) return $this->instances[strtolower($component)];
 		
@@ -41,7 +42,7 @@ class Dice {
 		
 		if (is_callable($callback, true)) call_user_func_array($callback, array($params));
 		
-		$reflection = new ReflectionClass($className);
+		$reflection = new \ReflectionClass($className);
 		$object = (count($params) > 0) ? $reflection->newInstanceArgs($params) : $object = new $className;
 		if ($rule->shared == true) $this->instances[strtolower($component)] = $object;
 		foreach ($rule->call as $call) call_user_func_array(array($object, $call[0]), $this->getMethodParams($className, $call[0], $rule, array_merge($this->getParams($call[1]), $args)));
@@ -50,15 +51,15 @@ class Dice {
 	
 	private function getParams(array $params = array(),array $newInstances = array()) {
 		for ($i = 0; $i < count($params); $i++) {
-			if ($params[$i] instanceof DiceInstance) $params[$i] = $this->create($params[$i]->name, array(), null, in_array(strtolower($params[$i]->name), array_map('strtolower', $newInstances)));
+			if ($params[$i] instanceof Instance) $params[$i] = $this->create($params[$i]->name, array(), null, in_array(strtolower($params[$i]->name), array_map('strtolower', $newInstances)));
 			else $params[$i] = ( !(is_array($params[$i]) && isset($params[$i][0]) && is_string($params[$i][0])) && is_callable($params[$i])) ? call_user_func($params[$i], $this) : $params[$i];
 		}
 		return $params;
 	}
 	
-	private function getMethodParams($className, $method, DiceRule $rule, array $args = array(), array $share = array()) {
+	private function getMethodParams($className, $method, Rule $rule, array $args = array(), array $share = array()) {
 		if (!method_exists($className, $method)) return array();
-		$reflectionMethod = new ReflectionMethod($className, $method);
+		$reflectionMethod = new \ReflectionMethod($className, $method);
 		$params = $reflectionMethod->getParameters();
 		$parameters = array();
 		foreach ($params as $param) {			
@@ -71,7 +72,7 @@ class Dice {
 				}
 			}
 			$paramClassName = $param->getClass() ? strtolower($param->getClass()->name) : false;			
-			if ($paramClassName && isset($rule->substitutions[$paramClassName])) $parameters[] = is_string($rule->substitutions[$paramClassName]) ? new DiceInstance($rule->substitutions[$paramClassName]) : $rule->substitutions[$paramClassName];
+			if ($paramClassName && isset($rule->substitutions[$paramClassName])) $parameters[] = is_string($rule->substitutions[$paramClassName]) ? new Instance($rule->substitutions[$paramClassName]) : $rule->substitutions[$paramClassName];
 			else if ($paramClassName && class_exists($paramClassName)) $parameters[] = $this->create($paramClassName, $share, null, in_array($paramClassName, array_map('strtolower', $rule->newInstances)));
 			else if (is_array($args) && count($args) > 0) $parameters[] = array_shift($args);
 			else $parameters[] = $param->isDefaultValueAvailable() ? $param->getDefaultValue() : null;			
@@ -80,7 +81,7 @@ class Dice {
 	}
 }
 
-class DiceRule {
+class Rule {
 	public $shared = false;
 	public $constructParams = array();
 	public $substitutions = array();
@@ -91,61 +92,10 @@ class DiceRule {
 	public $shareInstances = array();
 }
 
-class DiceInstance {
+class Instance {
 	public $name;
 	public function __construct($instance) {
 		$this->name = $instance;
 	}
 }
 
-class DiceXmlCallback {
-	private $str;
-
-	public function __construct($str) {
-		$this->str = $str;
-	}
-
-	public function create(Dice $dic) {
-		$parts = explode('::', trim($this->str, '{}'));
-		$object = $dic->create(array_shift($parts));
-		while ($var = array_shift($parts)) {
-			if (strpos($var, '(') !== false) {
-				$args = explode(',', substr($var, strpos($var, '(')+1, strpos($var, ')')-strpos($var, '(')-1));
-				$object = call_user_func_array(array($object, substr($var, 0, strpos($var, '('))), ($args[0] == null) ? array() : $args);
-			}
-			else $object = $object->$var;
-		}
-		return $object;
-	}
-}
-
-class DiceXmlLoader {
-	private function getComponent($str, $createInstance = false) {
-		if ($createInstance) return (strpos((string) $str, '{') === 0) ? array(new DiceXmlCallback($str), 'create') : new DiceInstance((string) $str);
-		else return (strpos((string) $str, '{') === 0) ? array(new DiceXmlCallback($str), 'create') : (string) $str;
-	}
-
-	public function loadXml($map, Dice $dic) {
-		if (!($map instanceof SimpleXmlElement)) $map = simplexml_load_file($map);
-		$rules = array();
-		foreach ($map as $key => $value) {
-			$rule = clone $dic->getRule((string) $value->name);				
-			$rule->shared = ($value->shared == 'true');
-			$rule->inherit = ($value->inherit == 'true');
-			if ($value->call) {
-				foreach ($value->call as $name => $call) {
-					$callArgs = array();
-					if ($call->params) 	foreach ($call->params->children() as $key => $param) 	$callArgs[] = $this->getComponent((string) $param, ($key == 'instance'));
-					$rule->call[] = array((string) $call->method, $callArgs);					
-				}
-			}
-			if ($value->instanceof) $rule->instanceOf = (string) $value->instanceof;
-			if ($value->newinstances) $rule->newInstances = explode(',', $value->newinstances);
-			if ($value->substitute) foreach ($value->use as $use) $rule->substitutions[(string) $use->as] = $this->getComponent((string) $use->use, true);
-			if ($value->construct) 	foreach ($value->construct->children() as $child) $rule->constructParams[] = $this->getComponent((string) $child);
-			if ($value->shareinstances) foreach ($value->shareinstances as $share) $rule->shareInstances[] = $this->getComponent((string) $share, true);
-			$dic->addRule((string) $value->name, $rule);
-		}
-	}
-}
-?>
