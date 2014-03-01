7938c0a45294c440796dea53c7ab8e3a47a32f76
Tom <tom@r.je>
2014-03-01 11:02:05 +0000

    Added unit tests for the XML loader and fixed a bug where the xml loader wasn't setting up rules with substitutions correctly.

---
+++ tests/xmlloadertest.php
@@ -0,0 +1,256 @@
+<?php
+/* @description 		Dice - A minimal Dependency Injection Container for PHP
+ * @author				Tom Butler tom@r.je
+* @copyright			2012-2014 Tom Butler <tom@r.je>
+* @link				http://r.je/dice.html
+* @license				http://www.opensource.org/licenses/bsd-license.php  BSD License
+* @version				1.1
+*/
+
+/*
+ *  N.B. Please ensure all tests in dicetest.php pass before testing the XML loader.
+ * 
+ */
+
+require_once '../dice.php';
+require_once '../xmlloader.php';
+
+
+class XmlLoaderTest extends PHPUnit_Framework_TestCase {
+	private $dice;
+	private $xmlLoader;
+
+	protected function setUp() {
+		parent::setUp ();
+		$this->dice = $this->getMock('\\Dice\\Dice', array('getRule', 'addRule'));		
+		$this->dice->expects($this->any())->method('getRule')->will($this->returnValue(new \Dice\Rule));
+		$this->xmlLoader = new \Dice\XML\Loader;
+	}
+
+	protected function tearDown() {
+		$this->dice = null;	
+		$this->xmlLoader = null;
+		parent::tearDown ();
+	}
+	
+	
+	public function testSetDefaultRule() {
+		$xml = '<?xml version="1.0"?>
+<dice>
+	<rule>
+		<name>*</name>
+		<shared>true</shared>
+	</rule>
+</dice>';
+		
+	
+		$equivalentRule = new \Dice\Rule;
+		$equivalentRule->shared = true;
+		
+		$this->dice->expects($this->once())->method('addRule')->with($this->equalTo('*'), $this->equalTo($equivalentRule));
+		$this->xmlLoader->loadXml(simplexml_load_string($xml), $this->dice);
+		
+	}
+	
+	public function testShared() {
+		$xml = '<?xml version="1.0"?>
+<dice>
+	<rule>
+		<name>A</name>
+		<shared>true</shared>
+	</rule>
+</dice>';
+		
+		
+		$equivalentRule = new \Dice\Rule;
+		$equivalentRule->shared = true;
+		
+		$this->dice->expects($this->once())->method('addRule')->with($this->equalTo('A'), $this->equalTo($equivalentRule));
+		$this->xmlLoader->loadXml(simplexml_load_string($xml), $this->dice);
+	}
+	
+	
+	
+	public function testConstructParams() {
+		$xml = '<?xml version="1.0"?>
+<dice>
+	<rule>
+		<name>A</name>
+		<construct>
+			<param>A</param>
+			<param>B</param>
+		</construct>
+	</rule>
+</dice>';
+	
+	
+		$equivalentRule = new \Dice\Rule;
+		$equivalentRule->constructParams[] = 'A';
+		$equivalentRule->constructParams[] = 'B';
+	
+		$this->dice->expects($this->once())->method('addRule')->with($this->equalTo('A'), $this->equalTo($equivalentRule));
+		$this->xmlLoader->loadXml(simplexml_load_string($xml), $this->dice);
+	}
+	
+	
+	public function testSubstitutions() {
+		$xml = '<?xml version="1.0"?>
+<dice>
+	<rule>
+		<name>A</name>
+		<substitute>
+			<use>C</use>
+			<as>B</as>
+		</substitute>
+	</rule>
+</dice>';
+	
+	
+		$equivalentRule = new \Dice\Rule;
+		$equivalentRule->substitutions['B'] = new \Dice\Instance('C');
+		
+	
+		$this->dice->expects($this->once())->method('addRule')->with($this->equalTo('A'), $this->equalTo($equivalentRule));
+		$this->xmlLoader->loadXml(simplexml_load_string($xml), $this->dice);
+	}
+	
+	
+	public function testSubstitutions2() {
+		$xml = '<?xml version="1.0"?>
+<dice>
+	<rule>
+		<name>A</name>
+		<substitute>
+			<use>C</use>
+			<as>B</as>
+		</substitute>
+				
+		<substitute>
+			<use>E</use>
+			<as>F</as>
+		</substitute>
+	</rule>
+</dice>';
+	
+	
+		$equivalentRule = new \Dice\Rule;
+		$equivalentRule->substitutions['B'] = new \Dice\Instance('C');
+		$equivalentRule->substitutions['F'] = new \Dice\Instance('E');
+	
+	
+		$this->dice->expects($this->once())->method('addRule')->with($this->equalTo('A'), $this->equalTo($equivalentRule));
+		$this->xmlLoader->loadXml(simplexml_load_string($xml), $this->dice);
+	}
+	
+	
+	public function testNewInstances() {
+		$xml = '<?xml version="1.0"?>
+<dice>
+	<rule>
+		<name>A</name>
+		<newinstance>C</newinstance>
+		<newinstance>D</newinstance>
+		<newinstance>E</newinstance>
+	</rule>
+</dice>';
+		
+		$equivalentRule = new \Dice\Rule;
+		$equivalentRule->newInstances = ['C', 'D', 'E'];	
+	
+		$this->dice->expects($this->once())->method('addRule')->with($this->equalTo('A'), $this->equalTo($equivalentRule));
+		$this->xmlLoader->loadXml(simplexml_load_string($xml), $this->dice);
+	}
+	
+	public function testInstanceOf() {
+		$xml = '<?xml version="1.0"?>
+<dice>
+	<rule>
+		<name>[C]</name>
+		<instanceof>C</instanceof>
+	</rule>
+</dice>';
+	
+		$equivalentRule = new \Dice\Rule;
+		$equivalentRule->instanceOf = 'C';
+	
+		$this->dice->expects($this->once())->method('addRule')->with($this->equalTo('[C]'), $this->equalTo($equivalentRule));
+		$this->xmlLoader->loadXml(simplexml_load_string($xml), $this->dice);
+	}
+	
+	
+	public function testCall() {
+		$xml = '<?xml version="1.0"?>
+<dice>
+	<rule>
+		<name>A</name>
+		<call>
+			<method>setFoo</method> 
+           <params> 
+                <param>Foo</param> 
+                <param>Bar</param> 
+            </params>
+		</call> 
+	</rule>
+</dice>';
+	
+		$equivalentRule = new \Dice\Rule;
+		$equivalentRule->call[] = ['setFoo', ['Foo', 'Bar']];
+	
+		$this->dice->expects($this->once())->method('addRule')->with($this->equalTo('A'), $this->equalTo($equivalentRule));
+		$this->xmlLoader->loadXml(simplexml_load_string($xml), $this->dice);
+	}
+	
+	
+
+	public function testInherit() {
+		$xml = '<?xml version="1.0"?>
+<dice>
+	<rule>
+		<name>A</name>
+		<inherit>true</inherit>
+	</rule>
+</dice>';
+	
+		$equivalentRule = new \Dice\Rule;
+		$equivalentRule->inherit = true;
+	
+		$this->dice->expects($this->once())->method('addRule')->with($this->equalTo('A'), $this->equalTo($equivalentRule));
+		$this->xmlLoader->loadXml(simplexml_load_string($xml), $this->dice);
+	}
+	
+	
+	public function testInherit2() {
+		$xml = '<?xml version="1.0"?>
+<dice>
+	<rule>
+		<name>A</name>
+		<inherit>false</inherit>
+	</rule>
+</dice>';
+	
+		$equivalentRule = new \Dice\Rule;
+		$equivalentRule->inherit = false;
+	
+		$this->dice->expects($this->once())->method('addRule')->with($this->equalTo('A'), $this->equalTo($equivalentRule));
+		$this->xmlLoader->loadXml(simplexml_load_string($xml), $this->dice);
+	}
+	
+	public function testShareInstance() {
+		$xml = '<?xml version="1.0"?>
+<dice>
+	<rule>
+		<name>A</name>
+		<shareinstance>C</shareinstance>
+		<shareinstance>D</shareinstance>
+	</rule>
+</dice>';
+	
+		$equivalentRule = new \Dice\Rule;
+		$equivalentRule->shareInstances = ['C', 'D'];
+		
+		$this->dice->expects($this->once())->method('addRule')->with($this->equalTo('A'), $this->equalTo($equivalentRule));
+		$this->xmlLoader->loadXml(simplexml_load_string($xml), $this->dice);
+	}
+	
+	
+}
--- xmlloader.php
+++ xmlloader.php
@@ -2,20 +2,20 @@
 /* @description 		Dice - A minimal Dependency Injection Container for PHP
  * @author				Tom Butler tom@r.je
 * @copyright			2012-2014 Tom Butler <tom@r.je>
-* @link				http://r.je/dice.html
+* @link					http://r.je/dice.html
 * @license				http://www.opensource.org/licenses/bsd-license.php  BSD License
-* @version				1.1
+* @version				1.1.1
 */
-namespace Dice;
+namespace Dice\XML;
 
-class XmlCallback {
+class Callback {
 	private $str;
 
 	public function __construct($str) {
 		$this->str = $str;
 	}
 
-	public function create(Dice $dic) {
+	public function create(\Dice\Dice $dic) {
 		$parts = explode('::', trim($this->str, '{}'));
 		$object = $dic->create(array_shift($parts));
 		while ($var = array_shift($parts)) {
@@ -29,19 +29,19 @@ class XmlCallback {
 	}
 }
 
-class XmlLoader {
+class Loader {
 	private function getComponent($str, $createInstance = false) {
-		if ($createInstance) return (strpos((string) $str, '{') === 0) ? array(new XmlCallback($str), 'create') : new Instance((string) $str);
-		else return (strpos((string) $str, '{') === 0) ? array(new XmlCallback($str), 'create') : (string) $str;
+		if ($createInstance) return (strpos((string) $str, '{') === 0) ? array(new Callback($str), 'create') : new \Dice\Instance((string) $str);
+		else return (strpos((string) $str, '{') === 0) ? array(new Callback($str), 'create') : (string) $str;
 	}
 
-	public function loadXml($map, Dice $dic) {
+	public function loadXml($map, \Dice\Dice $dic) {
 		if (!($map instanceof \SimpleXmlElement)) $map = simplexml_load_file($map);
 		$rules = array();
 		foreach ($map as $key => $value) {
 			$rule = clone $dic->getRule((string) $value->name);
 			$rule->shared = ($value->shared == 'true');
-			$rule->inherit = ($value->inherit == 'true');
+			$rule->inherit = ($value->inherit == 'false') ? false : true;
 			if ($value->call) {
 				foreach ($value->call as $name => $call) {
 					$callArgs = array();
@@ -50,10 +50,10 @@ class XmlLoader {
 				}
 			}
 			if ($value->instanceof) $rule->instanceOf = (string) $value->instanceof;
-			if ($value->newinstances) $rule->newInstances = explode(',', $value->newinstances);
-			if ($value->substitute) foreach ($value->use as $use) $rule->substitutions[(string) $use->as] = $this->getComponent((string) $use->use, true);
+			if ($value->newinstance) foreach ($value->newinstance as $ni) $rule->newInstances[] = (string) $ni;
+			if ($value->substitute) foreach ($value->substitute as $use) 	$rule->substitutions[(string) $use->as] = $this->getComponent((string) $use->use, true);
 			if ($value->construct) 	foreach ($value->construct->children() as $child) $rule->constructParams[] = $this->getComponent((string) $child);
-			if ($value->shareinstances) foreach ($value->shareinstances as $share) $rule->shareInstances[] = $this->getComponent((string) $share, true);
+			if ($value->shareinstance) foreach ($value->shareinstance as $share) $rule->shareInstances[] = $this->getComponent((string) $share, false);
 			$dic->addRule((string) $value->name, $rule);
 		}
 	}
