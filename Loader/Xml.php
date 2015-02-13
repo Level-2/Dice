82d65f31f8e4e93b812fb01d0924c50c6ef372a1
Tom <tom@r.je>
2015-02-13 11:35:03 +0000

    loaders can now act as factories

--- Loader/Xml.php
+++ Loader/Xml.php
@@ -1,20 +1,20 @@
 <?php 
 /* @description 		Dice - A minimal Dependency Injection Container for PHP
- * @author				Tom Butler tom@r.je
+* @author				Tom Butler tom@r.je
 * @copyright			2012-2014 Tom Butler <tom@r.je>
 * @link					http://r.je/dice.html
 * @license				http://www.opensource.org/licenses/bsd-license.php  BSD License
 * @version				1.1.1
 */
 namespace Dice\Loader;
-
 class XML {
 	private function getComponent($str, $createInstance = false) {
 		if ($createInstance) return (strpos((string) $str, '{') === 0) ? [new Callback($str), 'run'] : new \Dice\Instance((string) $str);
 		else return (strpos((string) $str, '{') === 0) ? [new Callback($str), 'run'] : (string) $str;
 	}
 
-	public function load($map, \Dice\Dice $dic) {
+	public function load($map, \Dice\Dice $dic = null) {
+		if ($dice === null) $dice = new \Dice\Dice;
 		if (!($map instanceof \SimpleXmlElement)) $map = simplexml_load_file($map);
 		foreach ($map as $key => $value) {
 			$rule = clone $dic->getRule((string) $value->name);
