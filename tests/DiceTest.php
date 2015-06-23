bc01a5921140984d99a8397f5031ac38bc6dcb94
Tom <tom@r.je>
2015-06-23 17:55:38 +0100

    Re-enabled autoloader with description + fixed copyright

--- tests/DiceTest.php
+++ tests/DiceTest.php
@@ -1,13 +1,13 @@
 <?php
-/* @description 		Dice - A minimal Dependency Injection Container for PHP
- * @author				Tom Butler tom@r.je
-* @copyright			2012-2014 Tom Butler <tom@r.je>
-* @link				http://r.je/dice.html
-* @license				http://www.opensource.org/licenses/bsd-license.php  BSD License
-* @version				1.1
+/*@description        Dice - A minimal Dependency Injection Container for PHP
+* @author             Tom Butler tom@r.je
+* @copyright          2012-2015 Tom Butler <tom@r.je>
+* @link               http://r.je/dice.html
+* @license            http://www.opensource.org/licenses/bsd-license.php  BSD License
+* @version            2.0
 */
 class DiceTest extends PHPUnit_Framework_TestCase {
-	private $dice;
+	protected $dice;
 
 	public function __construct() {
 		parent::__construct();
@@ -15,7 +15,11 @@ class DiceTest extends PHPUnit_Framework_TestCase {
 	}
 	
 	public function autoload($class) {
-	//	$this->fail('Autoload triggered: ' . $class);
+		//If Dice Triggers the autoloader the test fails
+		//This generally means something invalid has been passed to 
+		//a method such as is_subclass_of or dice is trying to construct
+		//an object from something it shouldn't.
+		$this->fail('Autoload triggered: ' . $class);
 	}
 	
 	protected function setUp() {
@@ -29,8 +33,7 @@ class DiceTest extends PHPUnit_Framework_TestCase {
 	}
 		
 	public function testNoConstructor() {
-		$a = $this->dice->create('NoConstructor');
-		
+		$a = $this->dice->create('NoConstructor');		
 		$this->assertInstanceOf('NoConstructor', $a);
 	}
 
