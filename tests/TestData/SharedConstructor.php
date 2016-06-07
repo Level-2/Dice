dd966b43c9789964ab80ad81d24ea8ce9c314673
Kirill "Nemoden" K <i@nemoden.com>
2016-06-07 11:49:22 +1000

    sharedConstructor option added

--- tests/TestData/SharedConstructor.php
+++ tests/TestData/SharedConstructor.php
@@ -1,8 +1,8 @@
 <?php
-/**
- * @author Kirill @Nemoden K. <kovalchuk@drom.ru>
- * $ Date: Tue 07 Jun 2016 11:31:59 AM VLAT $
- */
+# First test-case. We are interested in getting data from cache if we created an object twice
+# with same constructor params using Dice
+
+namespace Test\SharedConstructor;
 
 class Greeter
 {
@@ -22,5 +22,88 @@ class Greeter
         }
         $this->cache[$name] = sprintf("%s, %s!", $this->greeting, $name);
         return $this->cache[$name];
+    }
+}
+
+# Second test case. We are interested in more complex sharedConstructor usage.
+# Namely, we want constructor to receive objects and closures so we can test
+# arguments are hashed correctly
+class BarBazShared
+{
+    private $barBaz;
+    private $clo;
+    public $id;
+
+    public function __construct(BarBaz $barBaz, \Closure $clo)
+    {
+        $this->barBaz = $barBaz;
+        $this->clo = $clo;
+        $this->id = microtime(); // should stay the same if instance is the same
+    }
+
+    public function getUid()
+    {
+    }
+
+    public function getBarBazWrapped()
+    {
+        $clo = $this->clo;
+        return $clo(sprintf(
+            "Bar: %s, baz: %s",
+            $this->barBaz->getBar()->value(),
+            $this->barBaz->getBaz()->value()
+        ));
+    }
+}
+
+class BarBaz
+{
+    private $bar;
+    private $baz;
+
+    public function __construct(Bar $bar, Baz $baz)
+    {
+        $this->bar = $bar;
+        $this->baz = $baz;
+    }
+
+    public function getBar()
+    {
+        return $this->bar;
+    }
+
+    public function getBaz()
+    {
+        return $this->baz;
+    }
+}
+
+class Bar
+{
+    private $bar;
+
+    public function __construct($bar)
+    {
+        $this->bar = $bar;
+    }
+
+    public function value()
+    {
+        return $this->bar;
+    }
+}
+
+class Baz
+{
+    private $baz;
+
+    public function __construct($baz)
+    {
+        $this->baz = $baz;
+    }
+
+    public function value()
+    {
+        return $this->baz;
     }
 }
