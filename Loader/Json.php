16513d53b393f21dc88ee6b3c981949503faddf7
Tom Butler <tom@r.je>
2017-05-01 14:08:18 +0100

    Restored Json.php
    Can be useful to avoid repeated error checking and quickly created configured dice instance

---
+++ Loader/Json.php
@@ -0,0 +1,24 @@
+<?php 
+/* @description     Dice - A minimal Dependency Injection Container for PHP         *  
+ * @author          Tom Butler tom@r.je                                             *
+ * @copyright       2012-2015 Tom Butler <tom@r.je> | http://r.je/dice.html         *
+ * @license         http://www.opensource.org/licenses/bsd-license.php  BSD License *
+ * @version         1.3.2                                                           */
+namespace Dice\Loader;
+class Json {
+	public function load($json, \Dice\Dice $dice = null) {
+		if ($dice === null) $dice = new \Dice\Dice;
+		if (trim($json)[0] != '{') {
+			$json =  file_get_contents($json);
+		}
+
+		$map = json_decode($json, true);
+
+		if (!is_array($map)) throw new \Exception('Could not decode json: ' . json_last_error_msg());
+
+
+		$dice->addRules($map);
+		
+		return $dice;
+	}
+}
