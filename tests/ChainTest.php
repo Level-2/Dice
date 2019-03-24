7471c102fb84c044fd524184e4d52247ab86306e
tom@r.je <tom@r.je>
2019-03-24 15:27:29 +0000

    #165 - chain tests

---
+++ tests/ChainTest.php
@@ -0,0 +1,96 @@
+<?php
+/* @description Dice - A minimal Dependency Injection Container for PHP *
+ * @author Tom Butler tom@r.je *
+ * @copyright 2012-2018 Tom Butler <tom@r.je> | https:// r.je/dice.html *
+ * @license http:// www.opensource.org/licenses/bsd-license.php BSD License *
+ * @version 3.0 */
+class ChainTest extends DiceTest {
+	public function testChainCall() {
+		$dice = $this->dice->addRules([
+				'$someClass' => [
+					'instanceOf' => 'Factory',
+					'call' => [
+						['get', [], \Dice\Dice::CHAIN_CALL]
+					]
+				]
+		]);
+
+		$obj = $dice->create('$someClass');
+
+		$this->assertInstanceOf('FactoryDependency', $obj);
+
+	}
+
+	public function testChainCallShared() {
+		$dice = $this->dice->addRules([
+				'$someClass' => [
+					'shared' => true,
+					'instanceOf' => 'Factory',
+					'call' => [
+						['get', [], \Dice\Dice::CHAIN_CALL]
+					]
+				]
+		]);
+
+		$obj = $dice->create('$someClass');
+
+		$this->assertInstanceOf('FactoryDependency', $obj);
+	}
+
+
+	public function testChainCallInject() {
+		$dice = $this->dice->addRules([
+				'FactoryDependency' => [
+					'instanceOf' => 'Factory',
+					'call' => [
+						['get', [], \Dice\Dice::CHAIN_CALL]
+					]
+				]
+		]);
+
+		$obj = $dice->create('RequiresFactoryDependecy');
+
+		$this->assertInstanceOf('FactoryDependency', $obj->dep);
+	}
+
+	public function testChainCallInjectShared() {
+		$dice = $this->dice->addRules([
+				'FactoryDependency' => [
+					'shared' => true,
+					'instanceOf' => 'Factory',
+					'call' => [
+						['get', [], \Dice\Dice::CHAIN_CALL]
+					]
+				]
+		]);
+
+		$obj = $dice->create('RequiresFactoryDependecy');
+
+		$this->assertInstanceOf('FactoryDependency', $obj->dep);
+
+		$obj2 = $dice->create('RequiresFactoryDependecy');
+
+		$this->assertNotSame($obj, $obj2);
+		$this->assertSame($obj->dep, $obj2->dep);
+	}
+
+}
+
+
+class Factory {
+	public function get() {
+		return new FactoryDependency;
+	}
+}
+
+class FactoryDependency {
+
+}
+
+class RequiresFactoryDependecy {
+	public $dep;
+
+	public function __construct(FactoryDependency $dep) {
+		$this->dep = $dep;
+	}
+}
