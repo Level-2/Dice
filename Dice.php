b9336d9200d0165c31e982374dc5d8d2552807bc
Tom Butler <tom@r.je>
2020-01-28 13:47:49 +0000

    Update version/copyright info

--- Dice.php
+++ Dice.php
@@ -1,9 +1,9 @@
 <?php
-/* @description Dice - A minimal Dependency Injection Container for PHP *
- * @author Tom Butler tom@r.je *
- * @copyright 2012-2018 Tom Butler <tom@r.je> | https:// r.je/dice.html *
- * @license http:// www.opensource.org/licenses/bsd-license.php BSD License *
- * @version 3.0 */
+/* @description Dice - A minimal Dependency Injection Container for PHP
+ * @author Tom Butler tom@r.je
+ * @copyright 2012-2020 Tom Butler <tom@r.je> | https://r.je/dice
+ * @license http://www.opensource.org/licenses/bsd-license.php BSD License
+ * @version 4.0 */
 namespace Dice;
 class Dice {
 	const CONSTANT = 'Dice::CONSTANT';
