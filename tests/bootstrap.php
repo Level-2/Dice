6a5414b1bd56a73919c9efaaf499ac40974bf9b3
Chris Seufert <chris@modd.com.au>
2021-03-26 11:26:03 +1100

    test coverage with phpuinit+coveralls

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,5 +4,5 @@
  * @copyright 2012-2018 Tom Butler <tom@r.je> | https:// r.je/dice.html *
  * @license http:// www.opensource.org/licenses/bsd-license.php BSD License *
  * @version 3.0 */
-require_once 'Dice.php';
+require_once 'src/Dice.php';
 require_once 'tests/DiceTest.php';
